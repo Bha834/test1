56203f3d149ca8e52219803c55f441dc1b641aad
Bhola Patidar <bholapatidar@gmail.com>
2025-07-30 14:37:39 +0530

    ne

--- app.py
+++ app.py
@@ -1,7 +1,4 @@
-<<<<<<< HEAD
 print("Hello from main branch - updated!")
-=======
 print('Hello from main branch')
 print("Greetings from feature branch!")
->>>>>>> feature-greet
 print("Hello my name is ------")
